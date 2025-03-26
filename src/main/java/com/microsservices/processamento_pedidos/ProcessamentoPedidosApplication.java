dc9ad7c992da710681ff8160cc648be14798e829
vinicinrn <zviniciusnascimento@gmail.com>
2025-03-26 19:20:01 -0300

    .

---
+++ src/main/java/com/microsservices/processamento_pedidos/ProcessamentoPedidosApplication.java
@@ -0,0 +1,13 @@
+package com.microsservices.processamento_pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessamentoPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessamentoPedidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microsservices/processamento_pedidos/ProcessamentoPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microsservices.processamento_pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessamentoPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
